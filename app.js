da50160589bef838df4659b524b5d5b4d3d4994f
grantb4812 <grantb4812@gmail.com>
2015-08-14 18:52:45 -0500

    making a commit

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  
+  var newLi = "<li class='add-li'><div class='add-remove-button'></i><i class='fa fa-check-square'></i><i class='fa fa-check-square-o'></i><i class='fa fa-minus-square-o'></i></div></li>"
+  
+
+
+$('.add-button').click(function(){
+      $('input').val(function(i,val){
+        $('.list-item').append(newLi);
+     $('.list-item  li').last().append(val);
+      })
+    });
+
+
+  $('input').keydown(function(enter){
+    if (enter.keyCode == 13) {
+      $('input').val(function(i,val){
+        $('.list-item').append(newLi);
+      $('.list-item  li').last().append(val);
+      });
+    }
+  });
+
+  $('.list-item').on('click', '.fa-minus-square-o', function(){
+    $(this).parent().parent().remove();
+  });
+  
+ $('.list-item').on('click', '.fa-check-square', function(){
+   $(this).hide();
+   $('.list-item li').addClass('line-through');
+   $('.fa-check-square-o').show();
+ }).on('click', '.fa-check-square-o', function(){
+   $(this).hide();
+   $('.list-item li').removeClass('line-through');
+   $('.fa-check-square').show();
+ })
+  
+});
+
